7b7113bca7af4723f5f8183fecee6d933550738d
Alfonso <fonsiAguilar@users.noreply.github.com>
2017-06-17 18:35:14 +0200

    inserted puts message

--- main.cpp
+++ main.cpp
@@ -4,11 +4,18 @@
  *  Created on: 17 Jun 2017
  *      Author: Alfonso
  */
+#include <stdio.h>
 #include <iostream>
 using namespace std;
+// this is from second branch
 
+void printSecondBranch()
+{
+  puts("secondBrach");
+}
 int main(void)
 {
+  printSecondBranch();
   cout<<"hello world!"<<endl;
   cout<<"second commit"<<endl;
   return 0;
