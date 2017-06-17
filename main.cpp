712f1cb9b12de716fdb81b4be85cfff0d55d90e9
Alfonso <fonsiAguilar@users.noreply.github.com>
2017-06-17 13:00:24 +0200

    first Commint hello

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 17 Jun 2017
+ *      Author: Alfonso
+ */
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+  cout<<"hello world!"<<endl;
+  return 0;
+}
+
